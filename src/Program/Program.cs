7a05e496794afed7a84b3cbc5c728df45180b2e0
Jessica-Mora <mora36702@hotmail.com>
2024-08-07 10:17:09 -0300

    resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,15 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+<<<<<<< Updated upstream
+=======
+
+// Esta clase implementa la operación <resta>.
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
+>>>>>>> Stashed changes
