10a8efc157566f28caac10da4b718d57c4ba4d1f
thiagocastillo <thiago.castillo.cbt@gmail.com>
2024-08-07 16:19:25 -0300

    multi

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,6 +2,13 @@ Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
 // implementación Division
 public class Division
 {
