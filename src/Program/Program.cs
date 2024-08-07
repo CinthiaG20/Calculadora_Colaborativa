89328c47f678fb7dda3f187fde3190bff52a2ef2
CinthiaG20 <cg9150153@gmail.com>
2024-08-07 10:12:05 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,7 +3,8 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 public class Addition
-{
+{ 
+    //implementacion de la operacion suma
     public static int Add(int a, int b)
     {
         return a + b;
