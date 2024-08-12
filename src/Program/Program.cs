b3e14ea0bff08be007e3f1fa29914971b5e3b078
Jessica-Mora <mora36702@hotmail.com>
2024-08-12 08:26:08 -0300

    cambios incorrectos 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,12 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
+﻿//Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
+//Console.WriteLine(Multiplication.Multiply(3, 3));
+//Console.WriteLine(Division.Divide(18, 2));
 public class Substraction
 {
     public static int Substract(int a, int b)
     {
-        return 0; //cambio incorrecto
+        return 0; //cambio incorrecto 
+        //cambios incorrectos
     }
 }
