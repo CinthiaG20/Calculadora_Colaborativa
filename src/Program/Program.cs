5a5014586fa92d5ee6e5c1f1020e3478e1d875dc
thiagocastillo <thiago.castillo.cbt@gmail.com>
2024-08-12 08:16:36 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,7 @@ public class Multiplication
 {
     public static int Multiply(int a, int b)
     {
-        return a * b;
+        return 0;// cambio incorrecto 
     }
 }
 // implementación Division
