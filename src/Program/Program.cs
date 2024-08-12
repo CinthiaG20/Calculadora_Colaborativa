4c2d7b7f97d58d9dbb6a4b15da1c755522ee4874
CinthiaG20 <cg9150153@gmail.com>
2024-08-12 08:43:55 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -35,6 +35,6 @@ public class Division
         //implementacion de la operacion suma
         public static int Add(int a, int b)
         {
-            return 0;
+            return a + b;
         }
     }
